9256a4d000fb7b10120bc1d9ceb9766d4bdcb576
Dimafsfs <123772044+Dimafsfs@users.noreply.github.com>
2023-09-26 14:33:15 +0100

    Add files via upload

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,16 @@
+print("Hello my name is Susan from space")
+print("What is your name")
+
+input1 = input("Enter name: ")
+print(f"hello {input1} I am from the year 2210 and I am 22 years old")
+
+print ("How old are you ?")
+number_1= input("Enter number")
+number_2= 2210
+int(number_1) + int(number_2)
+result= float(number_1) + float(number_2)
+print(f'Wow, by 2210 you will be {result} that is really old')
+print('What music are you in to?')
+input3=input("Enter music type")
+print(f"I have no heard of {input3}")
+
